4671be373234576d0bd44dadfb5916fb5aa96d0e
razmy <mohamedrazmy@efuturesworld.com>
2022-08-13 13:35:24 +0530

    initial commit

---
+++ src/main/java/lk/quadrate/quadqueue/QuadQueueApplication.java
@@ -0,0 +1,13 @@
+package lk.quadrate.quadqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadQueueApplication.class, args);
+    }
+
+}
